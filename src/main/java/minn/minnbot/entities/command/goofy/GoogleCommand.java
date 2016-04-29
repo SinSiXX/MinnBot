97bc77da9c1b2fe61a3e034ae088abc0cf554828
connor68989 <connorharkness99@hotmail.com>
2016-04-29 17:45:22 +0100

    Update GoogleCommand.java

--- src/main/java/minn/minnbot/entities/command/goofy/GoogleCommand.java
+++ src/main/java/minn/minnbot/entities/command/goofy/GoogleCommand.java
@@ -16,11 +16,11 @@ public class GoogleCommand extends CommandAdapter {
     @Override
     @SuppressWarnings( "deprecation" )
     public void onCommand(CommandEvent event) {
-        if(event.arguments.length < 1) {
+        if(event.arguments.length == 1) {
             event.sendMessage("Missing arguments. Usage: " + usage());
             return;
         }
-        if(event.arguments.length != 0){
+        if(event.arguments.length > 0){
         	event.sendMessage("http://lmgtfy.com/?q=" + URLEncoder.encode(event.allArguments));
         }
 }
