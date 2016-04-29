2ab2abfaf51ac476ac47788aaf3b2a438d311f2f
connor68989 <connorharkness99@hotmail.com>
2016-04-29 17:12:46 +0100

    Create GoogleCommand.java

---
+++ src/main/java/minn/minnbot/entities/command/goofy/GoogleCommand.java
@@ -0,0 +1,46 @@
+package minn.minnbot.entities.command.goofy;
+
+import java.net.URLEncoder;
+
+import minn.minnbot.entities.Logger;
+import minn.minnbot.events.CommandEvent;
+import minn.minnbot.entities.Logger;
+import minn.minnbot.entities.command.listener.CommandAdapter;
+
+public class GoogleCommand extends CommandAdapter {
+	public GoogleCommand(String prefix, Logger logger) {
+        this.prefix = prefix;
+        this.logger = logger;
+    }
+
+    @Override
+    @SuppressWarnings( "deprecation" )
+    public void onCommand(CommandEvent event) {
+        if(event.arguments.length < 1) {
+            event.sendMessage("Missing arguments. Usage: " + usage());
+            return;
+        }
+        if(event.arguments.length == 1){
+        	
+        	
+        	event.sendMessage("http://lmgtfy.com/?q=" + URLEncoder.encode(event.allArguments));
+        }
+        if(event.arguments.length >= 2){
+        	event.sendMessage("Too many arguments. Usage:"+usage());
+        }
+}
+
+    public boolean isCommand(String message) {
+    	String[] p = message.split(" ",2);
+        return p.length > 0 && p[0].equalsIgnoreCase(prefix + "lmgtfy");
+      }
+
+    public String usage() {
+        return prefix+"lmgtfy <query> | Returns a let me google that for you link with the added query";
+    }
+
+    @Override
+    public String getAlias() {
+        return "lmgtfy <query>";
+    }
+}
