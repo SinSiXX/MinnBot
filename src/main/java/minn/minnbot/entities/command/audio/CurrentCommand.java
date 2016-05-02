853d571a19c12b699f33de1e8df32fbd40a0a4e9
MinnDevelopment <minn69minn@gmail.com>
2016-05-02 17:20:29 +0200

    Updated copyrole command for better results

--- src/main/java/minn/minnbot/entities/command/audio/CurrentCommand.java
+++ src/main/java/minn/minnbot/entities/command/audio/CurrentCommand.java
@@ -8,6 +8,7 @@ import net.dv8tion.jda.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.player.MusicPlayer;
 import net.dv8tion.jda.player.source.AudioInfo;
 import net.dv8tion.jda.player.source.AudioSource;
+import net.dv8tion.jda.player.source.AudioTimestamp;
 
 import java.util.List;
 
@@ -19,7 +20,7 @@ public class CurrentCommand extends CommandAdapter {
     }
 
     public void onMessageReceived(MessageReceivedEvent event) {
-        if(event.isPrivate())
+        if (event.isPrivate())
             return;
         super.onMessageReceived(event);
     }
@@ -30,6 +31,7 @@ public class CurrentCommand extends CommandAdapter {
         List<AudioSource> playlist = player.getAudioQueue();
         AudioSource previous = player.getPreviousAudioSource();
         AudioSource current = player.getCurrentAudioSource();
+        int totalSeconds = 0;
         String s = "";
         if (previous != null) {
             AudioInfo info = previous.getInfo();
@@ -43,6 +45,7 @@ public class CurrentCommand extends CommandAdapter {
         }
         if (!player.isStopped() && current != null) {
             AudioInfo info = current.getInfo();
+            totalSeconds += current.getInfo().getDuration().getTotalSeconds() - player.getCurrentTimestamp().getTotalSeconds();
             if (info != null) {
                 try {
                     s += "**__Currently:__** `[" + player.getCurrentTimestamp().getTimestamp() + " / " + info.getDuration().getTimestamp() + "] " + (info.getTitle()).replace("`", "\u0001`").replace("[", "(").replace("]", ")") + "`\n";
@@ -54,17 +57,22 @@ public class CurrentCommand extends CommandAdapter {
             }
         }
         if (playlist.isEmpty()) {
-            s += "**__No queued songs.__**";
+            s += "**__No queued songs.__**\n";
         } else {
             int index = 1;
+            boolean passedMax = false;
             s += "**__Queue:__ " + playlist.size() + " songs** ```md\n";
             for (AudioSource f : playlist) {
+                totalSeconds += f.getInfo().getDuration().getTotalSeconds();
                 if (index > 5) {
-                    s += "...";
-                    break;
+                    if (!passedMax) {
+                        s += "...";
+                        passedMax = true;
+                    }
+                    continue;
                 }
                 AudioInfo info = f.getInfo();
-                if(info != null) {
+                if (info != null) {
                     try {
                         s += "[" + info.getDuration().getTimestamp() + "][" + (info.getTitle()).replace("`", "\u0001`").replace("[", "(").replace("]", ")") + "]\n";
                     } catch (NullPointerException ignored) {
@@ -75,8 +83,10 @@ public class CurrentCommand extends CommandAdapter {
                     index++;
                 }
             }
-            s += "```";
+            s += "``` ";
         }
+        String totalTimestamp = AudioTimestamp.fromSeconds(totalSeconds).getTimestamp();
+        s += "**Total length: " + totalTimestamp + "**\n";
         event.sendMessage(s);
     }
 
